6d90df0a0652d300aae1fa95e344796f1e06d7d7
Pandxra <83519905+Pandxra@users.noreply.github.com>
2022-03-23 13:08:48 -0300

    Tarefa Geral- Menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const mobile = document.getElementById('btn_mobile');
+function Menu () {
+    const nav = document.getElementById('menu');
+    nav.classList.toggle('active');
+}
+mobile.addEventListener('click', Menu);
