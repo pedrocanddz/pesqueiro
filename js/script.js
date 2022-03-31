8d15de5521f61216686964315134c39fd0a62272
pedrocanddz <pedrocanddz07@gmail.com>
2022-03-30 23:34:59 -0300

    eventos realizados

---
+++ js/script.js
@@ -0,0 +1,36 @@
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "eventos",
+		time: 5000,
+		nav: true
+	});
+
+}
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "noite_arabe",
+		time: 5000,
+		nav: true
+	});
+
+}
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "costela_fogo",
+		time: 5000,
+		nav: true
+	});
+
+}
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "buteco_dos_amigos",
+		time: 5000,
+		nav: true
+	});
+
+}
